5fb9cf33ab23d16b4528e8a50b46828955fd2fe7
saisharantej9-ai <saisharantej9@gmail.com>
2026-01-20 19:45:00 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("addition is:", a+b)
+print("subtraction is:", a-b)
+print("multiplication is:", a*b)
+print("division is:", a/b)
